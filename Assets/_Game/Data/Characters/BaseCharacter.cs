eb56905f6441f50a2e6df2487955537b968cf696
DavSanReq <davsanreq@alu.edu.gva.es>
2025-12-18 18:53:34 +0100

    Cambio en la estrucura de personajes y enemigos
    Ahora tanto los personajes como los enemigos heredan de una clase común.

--- Assets/_Game/Data/Characters/BaseCharacter.cs
+++ Assets/_Game/Data/Characters/BaseCharacter.cs
@@ -1,13 +1,7 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NuevoPersonaje", menuName = "ScriptableObjects/Personaje")]
-public class BaseCharacter : ScriptableObject
+public class BaseCharacter : EntityBase
 {
-    public int id;
-    public string characterName;
-    public Sprite sprite;
-    public int life;
-    public int energy;
-    public int force;
-    public int recovery;
+   
 }
---
+++ Assets/_Game/Data/Characters/EntityBase.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class NewEmptyCSharpScript : ScriptableObject
+{
+    public int id;
+    public new string name;
+    public Sprite sprite;
+    public int life;
+    public int energy;
+    public int force;
+    public int recovery;
+}
