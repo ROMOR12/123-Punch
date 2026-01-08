1d47ba91ed7fac1a29c8971ff35007a789cf5d91
Miguel Torregrosa López <migtorlop@alu.edu.gva.es>
2026-01-08 19:26:22 +0100

    Creación de scripts iniciales
    Preparación de ciertos scripts y creación del objeto modo de juego

---
+++ Assets/_Game/Scripts/UI/MenuPrincipal/ModosDeJuego.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+[CreateAssetMenu(fileName ="NuevoModoDeJuego",menuName ="ModoDeJuego")]
+public class ModosDeJuego : ScriptableObject
+{
+    public GameObject ModoDeJuego;
+
+    public Sprite Imagen;
+
+    public string Nombre;
+
+}
