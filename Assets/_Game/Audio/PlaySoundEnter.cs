4d2f3a9cca69a018767c83a19bf9503ae820eee8
DavSanReq <davsanreq@alu.edu.gva.es>
2026-01-12 18:41:56 +0100

    Añadido SoundManager
    -Se añade SoundManager.
    -Se añade PlaySoundEnter y Exit para añadir los sonidos.

---
+++ Assets/_Game/Audio/PlaySoundEnter.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlaySoundEnter : StateMachineBehaviour
+{
+    [SerializeField] private SoundType sound;
+    [SerializeField, Range(0, 1)] private float volume = 1;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        SoundManager.PlaySound(sound, volume);
+    }
+}
---
+++ Assets/_Game/Audio/PlaySoundExit.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlaySoundExit : StateMachineBehaviour
+{
+    [SerializeField] private SoundType sound;
+    [SerializeField, Range(0, 1)] private float volume = 1;
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        SoundManager.PlaySound(sound, volume);
+    }
+}
