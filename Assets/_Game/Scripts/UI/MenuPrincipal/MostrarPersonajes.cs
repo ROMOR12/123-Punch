19b7e14b7931cf1e2af726e58882b8469daeb136
Miguel Torregrosa López <migtorlop@alu.edu.gva.es>
2026-01-28 17:57:39 +0100

    Interfaz de personaje
    La interfaz de personaje ahora es funcional

--- Assets/_Game/Scripts/UI/MenuPrincipal/MostrarPersonajes.cs
+++ Assets/_Game/Scripts/UI/MenuPrincipal/MostrarPersonajes.cs
@@ -1,15 +1,74 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MostrarPersonajes : MonoBehaviour
 {
     private GameManager gameManager;
 
-    private 
-    public void Start()
+    [SerializeField]
+    private Image imagenPersonajeSeleccionado;
+
+    [SerializeField]
+    private Image imagenSeleccionPersonaje;
+
+    [SerializeField]
+    private TextMeshProUGUI textoVida;
+
+    [SerializeField]
+    private TextMeshProUGUI textoStam;
+
+    [SerializeField]
+    private TextMeshProUGUI textoDanyo;
+
+    [SerializeField]
+    private TextMeshProUGUI textoRecuperacion;
+
+    [SerializeField]
+    private TextMeshProUGUI textoNombrePersonaje;
+
+    private int indicePersonajeSeleccionado = 0;
+
+    void Start()
     {
         gameManager = GameManager.Instance;
 
+        ActualizarPersonaje();
     }
 
-    
+    private void ActualizarPersonaje()
+    {
+        imagenPersonajeSeleccionado.sprite = gameManager.listaPersonajes[indicePersonajeSeleccionado].sprite !=null? gameManager.listaPersonajes[indicePersonajeSeleccionado].sprite : gameManager.imageDefault;
+        imagenSeleccionPersonaje.sprite = gameManager.listaPersonajes[indicePersonajeSeleccionado].sprite != null ? gameManager.listaPersonajes[indicePersonajeSeleccionado].sprite : gameManager.imageDefault;
+
+        textoVida.text = gameManager.listaPersonajes[indicePersonajeSeleccionado].life.ToString();
+        textoStam.text = gameManager.listaPersonajes[indicePersonajeSeleccionado].energy.ToString();
+        textoDanyo.text = gameManager.listaPersonajes[indicePersonajeSeleccionado].force.ToString();
+        textoRecuperacion.text = gameManager.listaPersonajes[indicePersonajeSeleccionado].recovery.ToString();
+        textoNombrePersonaje.text = gameManager.listaPersonajes[indicePersonajeSeleccionado].entityName;
+    }
+
+    public void CambiarPersonajeBotonIzquierdo()
+    {
+        if (indicePersonajeSeleccionado == 0)
+            indicePersonajeSeleccionado = gameManager.listaPersonajes.Count - 1;
+        else
+            indicePersonajeSeleccionado--;
+        ActualizarPersonaje();
+    }
+    public void CambiarPersonajeBotonDerecho()
+    {
+        if (indicePersonajeSeleccionado == gameManager.listaPersonajes.Count - 1)
+            indicePersonajeSeleccionado = 0;
+        else
+            indicePersonajeSeleccionado++;
+        ActualizarPersonaje();
+    }
+
+  
+
+
+
+
+
 }
